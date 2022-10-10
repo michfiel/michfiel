4ee743df4b2d2a0e349c5f4808dc347c5b41f12f
Michael Fielitz <m.fielitz@axaio.com>
2022-10-10 12:50:09 +0200

    readded folder

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by m.fielitz on 2022-10-10
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello, World!\n";
+ 
+    
+    
+    
+    
+    return 0;
+}
