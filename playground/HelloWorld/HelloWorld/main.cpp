248b6994e0419ea35aa1a5abf08c77157806e303
michfiel <115180532+michfiel@users.noreply.github.com>
2022-10-10 12:30:44 +0200

    Added files via upload
    It's just a test

---
+++ playground/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by m.fielitz on 2022-10-10
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
